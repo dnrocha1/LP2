3f79dd3301dcef946417f4b9ff8c52f6902c5e84
dnrocha1 <dn.rocha@outlook.com>
2015-05-26 17:04:12 -0300

    Passo 1

--- Lab05/src/steam/Jogo.java
+++ Lab05/src/steam/Jogo.java
@@ -2,4 +2,23 @@ package steam;
 
 public class Jogo {
 
+	private String nome;
+	private double preco;
+	private int maiorScore;
+	private int vezesJogadas;
+	private int vezesZerou;
+	//existem tres tipos de Jogos ​, o RPG (Role Playing Game), Luta e Plataforma.
+
+	public Jogo() {
+		this.maiorScore = 0;
+		this.vezesJogadas = 0;
+		this.vezesZerou = 0;
+	}
+
+	private void joga(int score, boolean zerouJogo) {
+		vezesJogadas++;
+		if (score > maiorScore)
+			maiorScore = score;
+
+	}
 }
