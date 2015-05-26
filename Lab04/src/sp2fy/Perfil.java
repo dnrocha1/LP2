52cf5e7a6c4e7746aef2b84c4b114fe3bd95158d
dnrocha1 <dn.rocha@outlook.com>
2015-05-26 16:49:46 -0300

    Perfil Update

--- Lab04/src/sp2fy/Perfil.java
+++ Lab04/src/sp2fy/Perfil.java
@@ -1,13 +1,17 @@
 package sp2fy;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Perfil {
 
 	private String nome;
 	private ArrayList<Album> albuns;
 	private ArrayList<Album> albunsFavoritos;
-
+	//private HashMap<String, ArrayList<Musica>> playlist;
+	private ArrayList<Playlist> playlists;
+	
 	public Perfil(String nomePerfil) {
 		this.nome = nomePerfil;
 		this.albuns = new ArrayList<Album>();
@@ -19,18 +23,68 @@ public class Perfil {
 	}
 
 	public boolean adicionaAosFavoritos(Album album) {
-		if (pertenceAlbuns(album)) {
+		if (pertenceAlbuns(album.getTitulo())) {
 			return this.albunsFavoritos.add(album);
 		}
 		return false;
 	}
 
-	public boolean pertenceAlbuns(Album album) {
-		return this.albuns.contains(album);
+	public boolean pertenceAlbuns(String nomeAlbum) {
+		for (Album album : this.albuns) {
+			if (album.getTitulo().equalsIgnoreCase(nomeAlbum))
+				return true;
+		}
+		return false;
 	}
+
+	public void adicionaPlaylist(String nomePlaylist, String nomeAlbum, int faixa) throws Exception {
+		ArrayList<Musica> listaMusicas;
+		Album outroAlbum;
+		if (!this.hasPlaylist(nomePlaylist)) {
+			this.playlist = new HashMap<String, ArrayList<Musica>>();
+			listaMusicas = new ArrayList<Musica>();
+			this.playlist.put(nomePlaylist, listaMusicas);
+		} else {
+			listaMusicas = this.playlist.get(nomePlaylist);
+		}
+		if (!this.pertenceAlbuns(nomeAlbum)) {
+			throw new Exception("Album nao pertence ao Perfil especificado");
+		} else {
+			/*
+			 * Se existir, pegue a faixa do álbum especificado na assinatura do
+			 * método e adicione no agrupamento resultante da operação em 1.
+			 */
+			outroAlbum = this.getAlbum(nomeAlbum);
+			this.playlist.get(nomePlaylist).add(outroAlbum.getMusica(faixa));
+		}
+	}
+
+//	public boolean hasPlaylist(String nomePlaylist) {
+//		return this.playlist.containsKey(nomePlaylist);
+//	}
 
 	public String getNome() {
 		return nome;
 	}
 
+	public ArrayList<Album> getAlbuns() {
+		return albuns;
+	}
+	
+	public int getIndiceAlbuns(String nomeAlbum){
+		int index = 0;
+		for (Album album : this.albuns) {
+			if (album.getTitulo().equalsIgnoreCase(nomeAlbum))
+				return index;
+			index++;
+		}
+		if(index == 0)
+			index = -1;
+		return index;
+	}
+	
+	public Album getAlbum(String nomeAlbum){
+		return this.albuns.get(this.getIndiceAlbuns(nomeAlbum));			
+	}
+		
 }
