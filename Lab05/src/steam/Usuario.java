62230f0f21554c23b8654f03b989173d5cd29d46
dnrocha1 <dn.rocha@outlook.com>
2015-05-26 18:35:42 -0300

    Usuario sem filhos

--- Lab05/src/steam/Usuario.java
+++ Lab05/src/steam/Usuario.java
@@ -2,18 +2,22 @@ package steam;
 
 import java.util.ArrayList;
 
-public abstract class Usuario {
+public class Usuario {
 	
 	private String nome;
 	private String login;
 	private ArrayList<Jogo> jogosComprados;
 	private double dinheiro;
 	
-	public Usuario(){
+	public Usuario(String nome, String login){
+		this.nome = nome;
+		this.login = login;
 		this.jogosComprados = new ArrayList<Jogo>();
 	}
 	
-	public abstract boolean compraJogo();
+	public boolean compraJogo(){
+		
+	}
 	
 	public void adicionaDinheiro(){
 		
