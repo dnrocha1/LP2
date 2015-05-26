656042f3544388260854c6dfc2a62bfd0f6fe5d0
dnrocha1 <dn.rocha@outlook.com>
2015-05-26 17:31:46 -0300

    Classes Passo 3

--- Lab05/src/steam/Usuario.java
+++ Lab05/src/steam/Usuario.java
@@ -1,5 +1,21 @@
 package steam;
 
-public class Usuario {
+import java.util.ArrayList;
 
+public abstract class Usuario {
+	
+	private String nome;
+	private String login;
+	private ArrayList<Jogo> jogosComprados;
+	private double dinheiro;
+	
+	public Usuario(){
+		this.jogosComprados = new ArrayList<Jogo>();
+	}
+	
+	public abstract boolean compraJogo();
+	
+	public void adicionaDinheiro(){
+		
+	}
 }
---
+++ Lab05/src/steam/Veterano.java
@@ -0,0 +1,5 @@
+package steam;
+
+public class Veterano extends Usuario {
+
+}
