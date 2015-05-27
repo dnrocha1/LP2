02f55a8d7a5fc22a8a999860cb808c0a93f94116
dnrocha1 <dn.rocha@outlook.com>
2015-05-27 13:42:08 -0300

    Class Jogabilidade [Enum]

--- Lab05/src/steam/Jogabilidade.java
+++ Lab05/src/steam/Jogabilidade.java
@@ -1,5 +1,11 @@
 package steam;
 
 public enum Jogabilidade {
-
+	ONLINE("Online"), OFFLINE("Offline"), MULTIPLAYER("Multiplayer"), COOPERATIVO(
+			"Cooperativo"), COMPETITIVO("Competitivo");
+	
+	private final String valorJogabilidade;
+	Jogabilidade(String valor){
+		valorJogabilidade = valor;
+	}
 }
