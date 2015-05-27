e04973dab17a1b0557bc20019fa88bb9e16111ce
dnrocha1 <dn.rocha@outlook.com>
2015-05-27 15:16:21 -0300

    Tirou o HashSet

--- Lab05/src/steam/Jogo.java
+++ Lab05/src/steam/Jogo.java
@@ -9,7 +9,7 @@ public class Jogo {
 	private int maiorScore;
 	private int vezesJogadas;
 	private int vezesZerou;
-	private HashSet<EstilosDeJogo> EstilosDeJogo;
+	private EstilosDeJogo estilosDeJogo;
 
 	public Jogo(String nome, double preco) throws Exception {
 		if (preco < 0)
@@ -19,7 +19,6 @@ public class Jogo {
 		this.maiorScore = 0;
 		this.vezesJogadas = 0;
 		this.vezesZerou = 0;
-		this.EstilosDeJogo = new HashSet<EstilosDeJogo>();
 	}
 
 	public void joga(int score, boolean zerouJogo) {
